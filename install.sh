25c5327f0c6874f6d20c97df311a725ccbd78e44
Jasper Jonker <jjasper.jonker@gmail.com>
2024-03-23 20:12:10 +0100

    Update install
    Signed-off-by: Jasper Jonker <jjasper.jonker@gmail.com>

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
-# Install spaceship prompt
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git $HOME/.oh-my-zsh/custom/themes/spaceship-prompt --depth=1
+# Install oh-my-zsh
+git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+
+# Install zsh autosuggestions
+git clone --depth=1 git@github.com:zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # Install ZSH syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-# Install bat
+# Install spaceship prompt
+git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git $HOME/.oh-my-zsh/custom/themes/spaceship-prompt
+
+# Install bat & zsh
 if command -v apt-get &> /dev/null; then
-  sudo apt update && sudo apt install -y bat
+  sudo apt update && sudo apt install -y bat zsh neovim
 fi
 
-# Install neovim
+# Install neovim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
